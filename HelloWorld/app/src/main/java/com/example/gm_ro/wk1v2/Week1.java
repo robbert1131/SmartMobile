a2761abc88267530d2c22a9a1338a3bf91938295
LAPTOP-073QCJ84\gm_ro <gm_robbertvd@hotmail.com>
2016-09-01 13:40:50 +0200

    Helloworld week1

---
+++ HelloWorld/app/src/main/java/com/example/gm_ro/wk1v2/Week1.java
@@ -0,0 +1,13 @@
+package com.example.gm_ro.wk1v2;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Week1 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_week1);
+    }
+}
